40591151b430b3cf932b4586a0b69a0e9eaae26f
Yorik <darkyacik@yandex.ru>
2024-05-10 02:57:50 +0700

    added comments

--- scripts/requests.go
+++ scripts/requests.go
@@ -6,12 +6,16 @@ import (
 	"net/http"
 )
 
+// Структура, ответственная за части запроса
 type RequestParts struct {
+	// Тип запроса
+	// (GET, POST, PUT, PATCH, DELETE)
 	Type string
 	URL  string
 	Body io.Reader
 }
 
+// Сборка частей запроса в полноценный запрос
 func (r *RequestParts) CreateRequest() *http.Request {
 	var req *http.Request
 	var err error
@@ -22,6 +26,7 @@ func (r *RequestParts) CreateRequest() *http.Request {
 	return req
 }
 
+// Отправка запроса
 func SendRequest(req *http.Request) *http.Response {
 	var resp *http.Response
 	var err error
@@ -33,6 +38,7 @@ func SendRequest(req *http.Request) *http.Response {
 	return resp
 }
 
+// Чтение ответа
 func ReadResponse(resp *http.Response) string {
 	defer resp.Body.Close()
 	var body []byte
