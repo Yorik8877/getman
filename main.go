fb1e1716c2bd507b085491a4364ee9df4a73983a
Yorik <darkyacik@yandex.ru>
2024-05-09 23:17:58 +0700

    starting web-server here for now

--- main.go
+++ main.go
@@ -1,31 +1,27 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"log"
-
-	"github.com/Yorik8877/gostman/scripts"
+	"html/template"
+	"net/http"
 )
 
+type User struct {
+	Name string
+	Age  uint8
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Go is super easy")
+	bob := User{"Bob", 25}
+	tmpl, _ := template.ParseFiles("views/home_page.html")
+	tmpl.Execute(w, bob)
+}
+
+func handeRequest() {
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
-	values := map[string]string{"foo1": "baz1"}
-	jsonData, err := json.Marshal(values)
-	if err != nil {
-		panic(err)
-	}
-
-	var rp scripts.RequestParts = scripts.RequestParts{
-		Type: "POST",
-		URL:  "https://httpbin.org/post",
-		Body: bytes.NewBuffer(jsonData),
-	}
-	// Полноценный реквест`
-	req := rp.CreateRequest()
-
-	// Отправка запроса и получение ответа
-	resp := scripts.SendRequest(req)
-
-	// Чтение ответа
-	log.Println(scripts.ReadResponse(resp))
+	handeRequest()
 }
